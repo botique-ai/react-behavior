29aa1ff72d56caac71adda673dc275e0e83a9a0b
Netanel Gilad <netanel@botique.ai>
2018-03-14 14:33:04 +0200

    feat(State): add deriveFromProps to State

--- packages/react-behavior/src/State.tsx
+++ packages/react-behavior/src/State.tsx
@@ -1,22 +1,30 @@
 import * as React from "react";
+import {Renderable} from "./Renderable";
 
 export type StateSpec = { state: any; setState: any };
 
 export type StateProps = {
   initialState?: any;
-  children(props: StateSpec): JSX.Element | null;
+  children: Renderable<StateSpec>;
+  deriveFromProps?: any;
 };
 
-export class State extends React.Component<StateProps, {}> {
+export class State extends React.PureComponent<StateProps, {}> {
   constructor(props: StateProps) {
     super(props);
     this.state = props.initialState;
   }
 
+  componentWillReceiveProps(nextProps) {
+      if (nextProps.deriveFromProps) {
+          this.setState(nextProps.deriveFromProps);
+      }
+  }
+
   render() {
-    return this.props.children({
-      state: this.state,
-      setState: this.setState.bind(this)
-    });
+    const ToRender = this.props.children;
+    return (
+        <ToRender state={this.state} setState={this.setState.bind(this)} />
+    );
   }
 }
